dd0ff840489d704310232489fae8acfc7cc14b3e
Ideasog01 <92227089+Ideasog01@users.noreply.github.com>
2023-06-05 16:52:20 +0100

    Revert "Join Room"
    This reverts commit 1c03d45f74cde28c1f259198214fee40b97525a7.

--- CardGame/Assets/Classes/Networking/NetworkController.cs
+++ CardGame/Assets/Classes/Networking/NetworkController.cs
@@ -1,38 +1,16 @@
 using Photon.Pun;
 using System;
-using Photon.Realtime;
 using UnityEngine;
 
 public class NetworkController : MonoBehaviourPunCallbacks
 {
     private void Start()
     {
-        Connect();
+        PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster()
     {
         Debug.Log("CONNECTED TO THE " + PhotonNetwork.CloudRegion + " SERVER!");
-    }
-
-    public void Connect()
-    {
-        PhotonNetwork.ConnectUsingSettings();
-    }
-
-    public void JoinRoom() //Via Inspector
-    {
-        PhotonNetwork.JoinRandomRoom();
-    }
-
-    public override void OnJoinRandomFailed(short returnCode, string message)
-    {
-        Debug.Log("Room Connection Failed");
-        PhotonNetwork.CreateRoom(null, new RoomOptions{MaxPlayers = 2 });
-    }
-
-    public override void OnJoinedRoom()
-    {
-        Debug.Log("Joined the Room!");
     }
 }
